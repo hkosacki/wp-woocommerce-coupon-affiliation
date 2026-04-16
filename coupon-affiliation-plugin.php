198906086f5d9ddf6e1778bd7b5d081ea05468b3
hkosacki <hkosacki@gmail.com>
2026-04-16 23:19:34 +0200

    Initial project setup

---
+++ coupon-affiliation-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Coupon Affiliation
+ * Description: System śledzenia sprzedaży dla ambasadorów na podstawie kodów rabatowych.
+ * Version: 0.0.1
+ * Author: HubLabs 
+ */
+
+// Zabezpieczenie przed bezpośrednim dostępem
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Tutaj Cursor zacznie pisać logikę w następnych krokach.
